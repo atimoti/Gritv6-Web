d207e68320667b4ceb1f386d1571b54fa0b7c8ea
rich9 <basile@ufl.edu>
2015-11-24 09:53:18 +0000

    i think i can

--- server.py
+++ server.py
@@ -4,18 +4,24 @@ from flask import (
     jsonify,
     render_template,
     request )
+from mongoengine import connect
 import pymongo
+import os
 
-connection = pymongo.MongoClient("mongodb://$IP")
-db = connection.grit
-games = db.games
+app = Flask(__name__)
 
-app = Flask(__name__, static_url_path="")
+if os.getenv('MONGOLAB_URI') is not None: # on Heroku
+    mongolab_uri = os.getenv('MONGOLAB_URI')
+    db = mongolab_uri[mongolab_uri.rfind('/')+1:] #extract the database name
+    connect(db, host=mongolab_uri)
+else: # on Cloud9
+    connect('mempydemo')
 
 @app.route('/')
 def index():
-    _game = games.find_one()
-    return render_template('index.html', game = _game)
+    return render_template('index.html')
     
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv('PORT', 8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
