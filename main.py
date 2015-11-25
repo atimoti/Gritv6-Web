5abad37fa843ad9821576abf0deb9f976013d983
rich9 <basile@ufl.edu>
2015-11-25 20:22:30 +0000

    push to heroku with mongodb

--- main.py
+++ main.py
@@ -11,24 +11,40 @@ import os
 app = Flask(__name__)
 app.debug = True
 
-_game = {"hmm":1}
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+# MONGOLAB_URI = 
 if os.getenv('MONGOLAB_URI'):
     _game = {"hello":1}
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
+
+
+# app = Flask(__name__)
+# app.debug = True
+
+# _game = {"hmm":1}
+
+# # MONGOLAB_URI = 
+# if os.getenv('MONGOLAB_URI'):
+#     _game = {"hello":1}
 
 # connection = pymongo.MongoClient('MONGOLAB_URI')
 # db = connection.grit
 # games = db.games
 
-# games.insert_one({"players": [1, 2]})
+# # games.insert_one({"players": [1, 2]})
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # games.insert({"players": [3, 4]})
-    # _game = games.find()
-    return render_template('index.html', game = _game)
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     # games.insert({"players": [3, 4]})
+#     # _game = games.find()
+#     return render_template('index.html', game = _game)
     
-if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
-
-
+# if __name__ == '__main__':
+#     app.run(port=8080, host='0.0.0.0')
