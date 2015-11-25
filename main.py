2ead7695e9e12a9b56cd8f7bcd06c22c7b3b633b
rich9 <basile@ufl.edu>
2015-11-25 18:21:14 +0000

    push to heroku with mongodb

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
-from flask import (
-    Flask,
-    abort,
-    jsonify,
-    render_template,
-    request )
-from mongoengine import connect
-import pymongo
+from flask import Flask
 import os
 
 app = Flask(__name__)
 app.debug = True
 
-_game = {"hmm":1}
-
-# MONGOLAB_URI = 
-if os.getenv('MONGOLAB_URI'):
-    _game = {"hello":1}
-
-connection = pymongo.MongoClient('MONGOLAB_URI')
-db = connection.grit
-games = db.games
-
-# games.insert_one({"players": [1, 2]})
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    # games.insert({"players": [3, 4]})
-    # _game = games.find()
-    return render_template('index.html', game = _game)
-    
+    return 'Hello MEMpy'
+
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+    port = int(os.getenv('PORT', 8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
+
+# from flask import (
+#     Flask,
+#     abort,
+#     jsonify,
+#     render_template,
+#     request )
+# from mongoengine import connect
+# import pymongo
+# import os
+
+# app = Flask(__name__)
+# app.debug = True
+
+# _game = {"hmm":1}
+
+# # MONGOLAB_URI = 
+# if os.getenv('MONGOLAB_URI'):
+#     _game = {"hello":1}
+
+# connection = pymongo.MongoClient('MONGOLAB_URI')
+# db = connection.grit
+# games = db.games
+
+# # games.insert_one({"players": [1, 2]})
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     # games.insert({"players": [3, 4]})
+#     # _game = games.find()
+#     return render_template('index.html', game = _game)
+    
+# if __name__ == '__main__':
+#     app.run(port=8080, host='0.0.0.0')
