976b876f98ce79d253b9b15eb2a41831a2694d52
rich9 <basile@ufl.edu>
2015-11-28 20:25:40 +0000

    commit to heroku

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ games.insert_one({"players": [8, 9]})
 
 @app.route('/')
 def index():
-    return render_template('index.html', game = "hello world!")
+    return render_template('index.html', game = {"_id" : "hello world!"})
     
 @app.route('/<gameID>')
 def game():
