dde1049f6c2a752e5d11bbdc30d86c0fd9e5a0fb
rich9 <basile@ufl.edu>
2015-11-29 15:43:47 +0000

    commit to heroku

---
+++ static/js/routers/router.js
@@ -0,0 +1,11 @@
+/* global grit, Backbone */
+
+grit.Router = Backbone.Router.extend({
+    routes: {
+        "game/:gameID" : "startGame"
+    },
+    
+    startGame : function (gameID){
+        console.log("todo get game with ID: " + gameID)
+    }
+})
