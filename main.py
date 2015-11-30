482e7bf9dde2278ef98c7c35f600743c2f4d9a24
rich9 <basile@ufl.edu>
2015-11-30 02:27:58 +0000

    commit to heroku

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ connection = pymongo.MongoClient(os.getenv('MONGOLAB_URI'))
 db = connection.grit
 games = db.games
 games.remove()
-games.insert_one({"_id":1, "players": [{"_id": "rich", "info": {"rating": 420, "gamesPlayed": 0, "cards": ['2S', '3S', '4S', 'AH']}}, {"_id": "anthony",  "info": {"rating": 1200, "gamesPlayed": 0, "cards": ['5S', '6S', '7S', 'AS']}}]})
+games.insert_one({"_id":1, "players": [{"_id": "rich", "info": {"rating": 1200, "gamesPlayed": 0, "cards": ['2S', '3S', '4S', 'AH']}}, {"_id": "anthony",  "info": {"rating": 1200, "gamesPlayed": 0, "cards": ['5S', '6S', '7S', 'AS']}}]})
 # games.insert_one({"players": [8, 9]})
 
 # _game = games.find_one()
@@ -29,7 +29,7 @@ def index():
 @app.route('/game/<int:gameID>', methods=['GET', 'POST'])
 def game(gameID):
     _game = games.find_one({"_id" : gameID})
-    return jsonify( _game )
+    return jsonify( **_game.to_json() )
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
