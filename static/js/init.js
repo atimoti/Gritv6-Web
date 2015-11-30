96ef7a24c3f0c1ffd2135c8cc7890f1d393ed0ee
rich9 <basile@ufl.edu>
2015-11-30 04:25:04 +0000

    commit to heroku

--- static/js/init.js
+++ static/js/init.js
@@ -1,5 +1,6 @@
-/*global grit*/
+/*global grit, Backbone*/
 
+Backbone.Model.prototype.idAttribute = "_id"; //allow Backbone to accpet MongoDB's '_id' attribute as opposed to Backbone's default 'id'
 var grit = grit || {};
 var settings = settings || {};
 
